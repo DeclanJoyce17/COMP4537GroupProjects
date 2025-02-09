dba731f849c57bdc036d3cff9e523f13aa2aab02
Snowballxld <65568323+Snowballxld@users.noreply.github.com>
2025-02-08 17:45:09 -0800

    Added basic server handling

---
+++ Lab4/server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const url = require('url');
+const port = process.env.PORT || 3000;
+
+
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+
+    if (req.method === 'GET' && path === '/api/definitions') {
+
+    } else if (req.method === 'POST' && path === '/api/definitions') {
+
+    } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: "Endpoint not found." }));
+    }
+
+});
+
+
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
