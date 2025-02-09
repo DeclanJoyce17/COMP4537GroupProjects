57a0d91040df345f28a215c7f1c5612d931796f3
Snowballxld <65568323+Snowballxld@users.noreply.github.com>
2025-02-09 01:25:39 -0800

    did more work on server handling

--- Lab4/server.js
+++ Lab4/server.js
@@ -2,13 +2,37 @@ const http = require('http');
 const url = require('url');
 const port = process.env.PORT || 3000;
 
+let dictionary = [];
+let requestCount = 0;
 
 const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
 
     if (req.method === 'GET' && parsedUrl.path === '/api/definitions') {
 
+        requestCount++;
+        const word = parsedUrl.query.word;
+
+
+
     } else if (req.method === 'POST' && parsedUrl.path === '/api/definitions') {
+        requestCount++;
+        const data = JSON.parse(body);
+        const word = data.word;
+        const definition = data.definition;
+
+        if (dictionary.includes(word.toLowerCase())) {
+            //Case for if dictionary already includes word
+            res.writeHead();
+            return res.end();
+        } else if (!dictionary.includes(word.toLowerCase())) {
+            //case for if dictionary doesn't include word
+
+            dictionary.push({ word, definition });
+
+            res.writeHead();
+            return res.end();
+        }
 
     } else {
         res.writeHead(404, { 'Content-Type': 'application/json' });
