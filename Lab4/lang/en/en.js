95300ff8be1bb1541a511063ccf68f087a78180a
Declan Joyce <144269131+DeclanJoyce17@users.noreply.github.com>
2025-02-09 23:07:25 -0800

    user facing messages test

--- Lab4/lang/en/en.js
+++ Lab4/lang/en/en.js
@@ -1,3 +1,11 @@
-module.exports = {
-
+export const MESSAGES = {
+    INVALID_INPUT: "Invalid input. Please provide a valid word.",
+    ADD_SUCCESS: "New word added successfully!",
+    ADD_CONFLICT: "Warning! Word already exists.",
+    ADD_ERROR: "Error adding the word.",
+    SEARCH_SUCCESS: "Word found!",
+    SEARCH_NOT_FOUND: "Word not found!",
+    SEARCH_ERROR: "Error retrieving the word.",
+    INVALID_WORD_INPUT: "Invalid input. Please provide a valid word.",
+    LOADING: "Loading...",
 };
