21e1584cc2780e62d2ae5578cd72b676ccc91d09
Snowballxld <65568323+Snowballxld@users.noreply.github.com>
2025-02-09 22:48:23 -0800

    Added back comments

--- Lab4/scripts/script.js
+++ Lab4/scripts/script.js
@@ -84,7 +84,8 @@ class Dictionary {
 }
 
 // Initialize the dictionary object **outside the class** so it’s globally available
-const dictionary = new Dictionary(`https://comp4537groupprojects.onrender.com/api/definitions`);
+const dictionary = new Dictionary(`${window.location.origin}/api/definitions`);
+
 
 // Ensure that `dictionary` is available when the page is loaded
 document.addEventListener("DOMContentLoaded", () => {
