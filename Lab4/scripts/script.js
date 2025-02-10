94cb06b7803252ea670dec53b64d1485a12e168d
Declan Joyce <144269131+DeclanJoyce17@users.noreply.github.com>
2025-02-09 17:50:35 -0800

    added the addWord function to script

--- Lab4/scripts/script.js
+++ Lab4/scripts/script.js
@@ -15,13 +15,39 @@ class Dictionary{
                     addbutton.addEventListener("click", () => this.addWord());
                 }
 
-                if (addbutton) {
+                if (searchbutton) {
                     addbutton.addEventListener("click", () => this.searchWord());
                 }
             });
         }
 
-        async addWord() {}
+        async addWord() {
+            const word = document.getElementById("word").value.trim();
+            const definition = document.getElementById("definition").value.trim();
+            const responseElement = document.getElementById("response");
+
+            //input validation checking
+            if (!word || !definition || !/^[a-zA-Z]+$/.test(word)) {
+                responseElement.innerText = "invalid input";
+                return;
+            }
+
+    
+            try {
+                const response = await fetch(this.ApiURL, {
+                    method: "POST",
+                    headers: {"Content-Type": "application/json"},
+                    body: JSON.stringify({word, definition})
+                });
+
+                const data = await response.json()
+                responseElement.innerText = data.message || "Error";
+
+            } catch(errorType) {
+                responseElement.innerText = error;
+            }
+
+        }
 
         async searchWord() {}
 }
